818d01f8c07de8b97aec54f83f1d5d3f28dcfa67
Varsha Bagla <varsha.rathi@lntinfotech.com>
2020-06-08 17:22:11 +0530

    Assignment - Added some basic components with styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { AppWarningAlertComponent } from './warningAlert/app-warning-alert.component';
 import { ServersComponent } from './servers/servers.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
+    AppWarningAlertComponent,
     ServersComponent,
-    WarningAlertComponent,
     SuccessAlertComponent
   ],
   imports: [
---
+++ src/app/warningAlert/app-warning-alert.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  templateUrl : './app-warning-alert.component.html',
+  styleUrls : ['./app-warning-alert.component.css']
+})
+export class AppWarningAlertComponent {
+}
