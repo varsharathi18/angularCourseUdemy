8a252a7bf59d5a9e10c2a3012a52b25bf35f2c24
Varsha Bagla <varsha.rathi@lntinfotech.com>
2020-06-09 12:22:30 +0530

    two way binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  allowNewServer = false;
+  serverCreationStatus = 'No Server was created!';
+  serverName = '' ;
+  constructor() {
+    setTimeout( () => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer() {
+    if (this.serverName !== '') {
+      this.serverCreationStatus = this.serverName + ' is created';
+    }
+  }
+
+  onUpdateServer(event: Event) {
+    this.serverName = (event.target as HTMLInputElement).value;
   }
 
 }
