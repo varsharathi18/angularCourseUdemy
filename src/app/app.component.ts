17b833b17e08b3da35bdbe29152e100b5c2db2ce
Varsha Bagla <varsha.rathi@lntinfotech.com>
2020-06-09 16:39:53 +0530

    Assignment for Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
-  styles : [`
-      h3 {
-        color : dodgerblue;
-      }
-    `]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  counts = [];
+  showLine = false;
+  onDisplay(){
+    this.showLine = !this.showLine;
+    this.counts.push(this.counts.length + 1);
+    // this.counts.push(new Date());
+  }
 }
